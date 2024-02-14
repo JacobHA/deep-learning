43d9913031a89a2f4966acbfe2f595a90a5a78c9
JacobHA <jacobhadamczyk@gmail.com>
2024-02-13 19:51:55 -0500

    cnn on cifar10

---
+++ Code/utils.py
@@ -0,0 +1,62 @@
+# Source: https://colab.research.google.com/drive/12YBDQFYXN8VruxKTfzDpbPsYFAEQceQP#scrollTo=4XYFwWhU8yEt
+# https://keras.io/examples/vision/mnist_convnet/
+import matplotlib.pyplot as plt
+from IPython.display import Image
+
+def show_images(images: list) -> None:
+    n: int = len(images)
+    f = plt.figure()
+    for i in range(n):        
+        f.add_subplot(1, n, i + 1)
+        plt.imshow(images[i], cmap='gray')
+    plt.show(block=True)
+    
+def show_image(image) -> None:
+    plt.imshow(image, cmap='gray')
+    plt.show(block=True)
+    
+def plot_images_labels_prediction(images,labels,prediction,idx,num=10):
+    fig=plt.gcf()
+    fig.set_size_inches(12, 14)
+    if num > 25: num=25
+    for i in range(0, num):
+        ax=plt.subplot(5, 5, i+1)
+        ax.imshow(images[idx], cmap='binary')
+        title="label=" + str(labels[idx])
+        if len(prediction) > 0:
+            title += ",predict=" + str(prediction[idx])
+        ax.set_title(title, fontsize=10)
+        ax.set_xticks([]);
+        ax.set_yticks([]);
+        idx += 1
+    plt.show()
+    
+def show_train_history(train_history):
+    fig=plt.gcf()
+    fig.set_size_inches(16, 6)
+    plt.subplot(121)
+    print(train_history.history.keys())
+    
+    if "accuracy" in train_history.history.keys():
+        plt.plot(train_history.history["accuracy"])
+    
+    if "val_accuracy" in train_history.history.keys():
+        plt.plot(train_history.history["val_accuracy"])
+        
+    plt.title("Train History")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend(["train", "validation"], loc="upper left")
+    plt.subplot(122)
+    
+    if "loss" in train_history.history.keys():
+        plt.plot(train_history.history["loss"])
+        
+    if "val_loss" in train_history.history.keys():
+        plt.plot(train_history.history["val_loss"])
+        
+    plt.title("Train History")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend(["train", "validation"], loc="upper left")
+    plt.show()
